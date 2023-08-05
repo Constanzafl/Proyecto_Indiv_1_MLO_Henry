7c6b06d1299e7257cce4c69db431ec6ebffa50be
Constanzafl <florioconstanza@gmail.com>
2023-08-05 12:24:29 -0300

    virtualenv

--- main.py
+++
@@ -1,7 +0,0 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI(title='Trabajo 1 MLO Henry Constanza Florio')
-
-
---
+++ myenv/main.py
@@ -0,0 +1,5 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI(title='Trabajo 1 MLO Henry Constanza Florio')
